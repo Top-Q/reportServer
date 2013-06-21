63292426e7afa79dbf9a7531f9dd6f2ac4ae8f48
lielran <liel.ran.maill@gmail.com>
2013-06-21 22:21:27 +0300

    add main method

--- server/report-service/src/main/java/il/co/topq/systems/report/component/utils/CheckIp.java
+++ server/report-service/src/main/java/il/co/topq/systems/report/component/utils/CheckIp.java
@@ -6,12 +6,21 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.UnknownHostException;
 
 public class CheckIp {
 
-	public static String getMyLanIp() throws Exception {
+	
+	
+	public static void main(String[] args) throws Exception {
+		 System.out.println(getMyLanIp());
+		 System.out.println(getMyWanIp());
+
+	}
+	
+	public static String getMyLanIp()  {
 
 		String localhost = "127.0.0.1";
 
@@ -25,11 +34,16 @@ public class CheckIp {
 
 	}
 
-	public static String getMyWanIp() throws Exception {
+	public static String getMyWanIp() throws IOException  {
 		URL whatismyip = new URL("http://checkip.amazonaws.com");
 		BufferedReader in = null;
 		try {
-			in = new BufferedReader(new InputStreamReader(whatismyip.openStream()));
+			try {
+				in = new BufferedReader(new InputStreamReader(whatismyip.openStream()));
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			String ip = in.readLine();
 			return ip;
 		} finally {
